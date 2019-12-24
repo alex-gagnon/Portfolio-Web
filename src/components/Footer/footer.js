00991ede55645158c3749930c225933d79dbdf2c
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2019-12-23 23:58:38 -0500

    Fixed github footer link

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,7 +5,7 @@ function footer() {
         <div id="footer" className="container-fluid">
             <div id="contact-footer">
                 <ul>
-                    <li><a href="https://github.com/bokoblin-link" className="contact-footer-item">Github</a></li>
+                    <li><a href="https://github.com/alex-gagnon" className="contact-footer-item">Github</a></li>
                     <li><a href="https://www.linkedin.com/in/agagnon313/" className="contact-footer-item">LinkedIn</a>
                     </li>
                     <li><a href="https://codepen.io/Alex_Gagnon/#" className="contact-footer-item">Codepen</a></li>
