3c0665fc52abbda706c8e4576f31bc8bdcd20a53
alex-gagnon <56893298+alex-gagnon-firebrand@users.noreply.github.com>
2020-01-12 20:57:31 -0500

    Set up initial footer header and main

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
+      <main className="App-main">
+        <Main />
+      </main>
+      <footer className="App-footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ app/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        )
+    }
+}
+
+export default Main;
