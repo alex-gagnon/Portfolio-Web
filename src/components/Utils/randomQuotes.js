37db8883dbe8425c3fdedcee9b65687aacd7f871
Alexander Gagnon <28638888+alex-gagnon@users.noreply.github.com>
2020-02-14 19:04:14 -0500

    updated to latest

---
+++ src/components/Utils/randomQuotes.js
@@ -0,0 +1,34 @@
+// 11. “Testers don’t like to break things; they like to dispel the illusion that things work.”— Kaner, Bach, Pettichord
+
+// 12. “Pretty good testing is easy to do (that’s partly why some people like to say ‘testing is dead’– they think testing isn’t needed as a special focus because they note that anyone can find at least some bugs some of the time). Excellent testing is quite hard to do.”— James Bach
+
+// 13. “A pinch of probability is worth a pound of perhaps.”— James Thurber
+
+// 14. “Testing is not responsible for the bugs inserted into software any more than the sun is responsible for creating dust in the air.”— Dorothy Graham
+
+// 15. “To those who say that “if you need testing at the end, you’re doing it wrong”, would you prefer a Boeing, or are you going Air Icarus?”— Michael Bolton
+
+// 16. “The problem is not that testing is the bottleneck. The problem is that you don’t know what’s in the bottle. That’s a problem that testing addresses.“— Michael Bolton
+
+// 17. “As ironic as it seems, the challenge of a tester is to test as little as possible. Test less, but test smarter.” — Federico Toledo
+
+// 18. “I am pretty sure there is a difference between “this has not been proven” and “this is false.”— Ron Jeffries
+
+// 19. “Testing is a skill. While this may come as a surprise to some people it is a simple fact.”— Fewster and Graham
+
+// 20. “You can be a great tester if you have programming skills. You can also be a great tester if you have no programming skills at all. And, you can be a lousy tester with or without programming skills. A great tester will learn what skills she needs to continue to be great, in her own style.”— Jerry Weinberg
+
+// 21. “No amount of testing can prove a software right, a single test can prove a software wrong.”— Amir Ghahrai
+
+// 22. “Discovering the unexpected is more important than confirming the known.“— George E. P. Box
+
+// 23. “The most exciting phrase to hear in science, the one that heralds discoveries, is not ‘Eureka!’ but ‘Now that’s funny…'”— Isaac Asimov
+
+// 24. “Testing is an infinite process of comparing the invisible to the ambiguous in order to avoid the unthinkable happening to the anonymous.”— James Bach
+
+const quotes = [
+    {quote: "", author: ""},
+    {quote: "We only see what we know.", author: "Goethe"}
+]
+
+// source: https://abstracta.us/blog/tools/ultimate-list-100-software-testing-quotes/
