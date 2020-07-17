7237b71ff8f250a5d1379b6e75956e9a23944fd0
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-17 00:37:44 -0400

    Updated tests to be in line with testing environment

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -3,44 +3,20 @@ import { render } from '@testing-library/react';
 import App from './components/App';
 
 
-test('renders hey there opener', () => {
+function verifyTextInDocument(text) {
   const { getByText } = render(<App />);
-  const textElement = getByText(/Hey there/i);
+  const textElement = getByText(text);
   expect(textElement).toBeInTheDocument();
+}
+
+test('renders app offline when api is unavailable', () => {
+  verifyTextInDocument(/APP OFFLINE/i)
 })
 
-test('renders intro tab', () => {  
-  const { getByText } = render(<App />);
-  const tabElement = getByText("INTRO");
-  expect(tabElement).toBeInTheDocument();
+test('renders testing quote when api is unavailable', () => {
+  verifyTextInDocument(/Testing Quote/i)
 })
 
-test('renders projects tab', () => {
-  const { getByText } = render(<App />);
-  const tabElement = getByText("PROJECTS");
-  expect(tabElement).toBeInTheDocument();
-})
-
-test('renders more tab', () => {
-  const { getByText } = render(<App />);
-  const tabElement = getByText("MORE");
-  expect(tabElement).toBeInTheDocument();
-})
-
-test('renders projects text when clicked', () => {
-  const { getByText } = render(<App />);
-  const tabElement = getByText("PROJECTS");
-  tabElement.click();
-  const monkeText = getByText(/Monk-E/i);
-  const mdText = getByText(/Markdown Previewer/i);
-  expect(monkeText).toBeInTheDocument(); 
-  expect(mdText).toBeInTheDocument();
-})
-
-test('renders more text when clicked', () => {
-  const { getByText } = render(<App />);
-  const tabElement = getByText("MORE");
-  tabElement.click();
-  const textElement = getByText(/My portfolio is currently undergoing a redesign/i);
-  expect(textElement).toBeInTheDocument();
+test('renders testing author when api is unavailable', () => {
+  verifyTextInDocument(/Testing Author/i)
 })
