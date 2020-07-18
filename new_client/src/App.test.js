ba44c92e50de5c1ce9d29b49d9b8bac672c1adc8
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-18 16:11:03 -0400

    Start designing the navigation menu and remove unused logos

--- new_client/src/App.test.js
+++ new_client/src/App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders home link', () => {
+  const { getByText } = render(<App />)
+  // Wait for timeout to finish for loading data
+  setTimeout(() => {
+    const linkElement = getByText(/Home/i)
+    expect(linkElement).toBeInTheDocument()
+  }, 2000)
+})
+
+test('renders about me link', () => {
+  const { getByText } = render(<App />)
+  // Wait for timeout to finish for loading data
+  setTimeout(() => {
+    const linkElement = getByText(/About me/i)
+    expect(linkElement).toBeInTheDocument()
+  }, 2000)
+})
