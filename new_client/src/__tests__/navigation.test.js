a140df0682d6c52005f81655ba7c226d48503d8d
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-18 16:30:34 -0400

    moved tests to __tests__. added projects to navigation

---
+++ new_client/src/__tests__/navigation.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('renders home link', () => {
+    const { getByText } = render(<App />)
+    // Wait for timeout to finish for loading data
+    setTimeout(() => {
+        const linkElement = getByText(/Home/i)
+        expect(linkElement).toBeInTheDocument()
+    }, 2000)
+})
+  
+test('renders about me link', () => {
+    const { getByText } = render(<App />)
+    // Wait for timeout to finish for loading data
+    setTimeout(() => {
+        const linkElement = getByText(/About me/i)
+        expect(linkElement).toBeInTheDocument()
+    }, 2000)
+})
+  
+test('renders projects link', () => {
+    const { getByText } = render(<App />)
+    // Wait for timeout to finish for loading data
+    setTimeout(() => {
+        const linkElement = getByText(/Projects/i)
+        expect(linkElement).toBeInTheDocument()
+    }, 2000)
+})
