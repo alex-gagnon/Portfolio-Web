fef44c4c33a712817a754ca91d0bff9683e5a12b
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-19 09:27:29 -0400

    general update

--- new_client/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders loading text', () => {
-  const { getByText } = render(<App />)
-  const textElement = getByText(/fetching data/i)
-  expect(textElement).toBeInTheDocument()
-})
