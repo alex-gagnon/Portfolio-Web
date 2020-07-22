98000de75121350db55f2a48b8c6a54b4d07be77
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-21 20:29:18 -0400

    Added additional tests. Added server tests to run in CI

--- server/src/controllers/quotes/index.js
+++ server/src/controllers/quotes/index.js
@@ -1,7 +1,7 @@
 const quotes = require('./quotes')
 
 const getAllQuotes = () => quotes
-const getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)]
+const getRandomQuote = () => quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)]
 const getSingleQuote = (id) => quotes.filter(obj => obj.id === parseInt(id))
 
 module.exports = {
