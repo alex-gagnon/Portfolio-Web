075181234c6948db9010738761196ecae62923dd
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-07-24 20:10:34 -0400

    Fix conflict 2

--- client/src/__tests__/home.test.js
+++ client/src/__tests__/home.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-<<<<<<< HEAD
-import Home from '../components/pages/Home';
-
-
-describe('renders home page without api connection', () => {
-    test('renders home page h2 header', () => {
-=======
 import { Home } from '../components/pages';
 
 
 describe('should render home page without api connection', () => {
     test('should render home page h2 header', () => {
->>>>>>> 51ea0120e61ada5cfcaaae687e1736057713156a
         const { getByText } = render(<Home />)
         const textElement = getByText("I cannot do that. I'll have to send in a maintenance request.")
         expect(textElement).toBeInTheDocument()
--- client/src/__tests__/more.test.js
+++ client/src/__tests__/more.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-<<<<<<< HEAD
-import More from '../components/pages/more';
-
-
-describe('renders more page without api connection', () => {
-    test('renders more page h2 header', () => {
-=======
 import { More } from '../components/pages';
 
 
 describe('should render more page without api connection', () => {
     test('should render more page h2 header', () => {
->>>>>>> 51ea0120e61ada5cfcaaae687e1736057713156a
         const { getByText } = render(<More />)
         const textElement = getByText("I do not have that information.")
         expect(textElement).toBeInTheDocument()
