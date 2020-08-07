6e38889f01abb6a5b5fd137a5758cf85b363d7f3
agagnon <28638888+bokoblin-link@users.noreply.github.com>
2020-08-06 20:36:58 -0400

    Default dark mode. Fixed favicon not appearing. Removed unused constructors

--- client/src/components/theme/theme.js
+++ client/src/components/theme/theme.js
@@ -1,23 +1,38 @@
-import React from "react"
+import React, { Component } from "react"
 import './theme.css'
 import ToggleButton from "react-bootstrap/ToggleButton"
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup"
 
-const toggleTheme = () => {
-    let theme = document.getElementById("Theme-container")
-    let currentTheme = theme.className
-    theme.className = currentTheme === "dark-mode" ? "light-mode" : "dark-mode"
-}
 
-const Theme = () => (
-    <ToggleButtonGroup id="toggle-btn__group" type="radio" name="theme-options" defaultValue="toggle-light" onChange={toggleTheme}>            
-        <ToggleButton id="toggle-btn__light" variant="outline-light" value="toggle-light">
-            Light
-        </ToggleButton>
-        <ToggleButton id="toggle-btn__dark" variant="outline-secondary" value="toggle-dark" style={{ "color": "white" }}>
-            Dark
-        </ToggleButton>
-    </ToggleButtonGroup>
-)
+class Theme extends Component {
+    constructor(props) {
+        super(props)
+
+        this.toggleTheme = this.toggleTheme.bind(this)
+    }
+
+    toggleTheme = () => {
+        let theme = document.getElementById("Theme-container")
+        let currentTheme = theme.className
+        theme.className = currentTheme === "dark-mode" ? "light-mode" : "dark-mode"
+    }
+
+    componentDidMount() {
+        this.toggleTheme()
+    }
+
+    render() {
+        return (
+            <ToggleButtonGroup id="toggle-btn__group" type="radio" name="theme-options" defaultValue="toggle-dark" onChange={this.toggleTheme}>            
+                <ToggleButton id="toggle-btn__light" variant="outline-light" value="toggle-light">
+                    Light
+                </ToggleButton>
+                <ToggleButton id="toggle-btn__dark" variant="outline-secondary" value="toggle-dark" style={{ "color": "white" }}>
+                    Dark
+                </ToggleButton>
+            </ToggleButtonGroup>
+        )
+    }
+}
 
 export default Theme
